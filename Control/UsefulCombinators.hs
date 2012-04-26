48598aeb91b9a412664265cc2f5024371b328dbf
Matthew Mirman <titaniumeyes@aol.com>
2012-04-26 19:05:05 -0400

    added two more combinators

--- Control/UsefulCombinators.hs
+++ Control/UsefulCombinators.hs
@@ -9,10 +9,25 @@
 -- Stability   :  experimental
 -- Portability :  portable
 -- 
--- Some useful combinators
+-- Some useful mostly simple combinators
 -----------------------------------------------------------------------------
 module Control.UsefulCombinators where
 
+-- | @'composeN' f n@ composes @f@ with itself @n@ times
+-- it has the invariant that @n '>=' 0@.
 composeN :: Num a => (b -> b) -> a -> b -> b
 composeN _ 0 = id
 composeN f i = f . composeN f (i-1)
+
+infixr 0 <<$> 
+-- | '<<$>' does the same thing as '<$>' or 'fmap' except that 
+-- it has the same fixity as '$'.  This is often useful.
+(<<$>) :: Functor f => (a -> b) -> f a -> f b
+(<<$>) = fmap
+
+-- | '-<$' can be used for peicing together functor bound results
+infixr 0 -<$
+(-<$) :: Functor f => (b -> c) -> f (a -> b) -> f (a -> c)
+(-<$) = fmap . (.)
+
+
